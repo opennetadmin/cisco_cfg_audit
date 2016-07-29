51e474dbe6b5dfd5f8b08cadf3f5f1e1d6986140
Matt Pascoe <matt@opennetadmin.com>
2016-07-29 14:00:54 -0700

    bump version number

--- plugin_info.php
+++ plugin_info.php
@@ -5,7 +5,7 @@ Your plugin must have a short description and a version number
 $plug_name = basename(dirname(__FILE__));
 
 $plugin_description = 'Compare IOS configuration to data stored in ONA';
-$plugin_version = '1.0';
+$plugin_version = '1.1';
 $plugin_help_url = 'http://github.com/opennetadmin/cisco_cfg_audit';
 
 
